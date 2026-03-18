446a75e5ff8911df88b371df6883cdcd8e1cb7cc
jayanthi-ranganathan <jai.ranga@gmail.com>
2026-03-18 17:21:04 +0530

    Add files via upload

---
+++ primitiveDataTypes.js
@@ -0,0 +1,12 @@
+var firstName = "Gemini";
+var companyName = "Google";
+var mobileNumber = 9876543210;
+var isAutomation = true;
+var hasPlaywright;
+
+console.log(typeof firstname);
+console.log(typeof firstName);      
+console.log(typeof companyName);    
+console.log(typeof mobileNumber);   
+console.log(typeof isAutomation);   
+console.log(typeof hasPlaywright);
