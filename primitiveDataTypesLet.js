a55211d9fb0a68df9e7a652eb3a4c9123f6b8824
jayanthi-ranganathan <jai.ranga@gmail.com>
2026-03-18 17:25:38 +0530

    Add files via upload

---
+++ primitiveDataTypesLet.js
@@ -0,0 +1,15 @@
+// Declare and Initialize Variables
+let userName = "Jayanthi";             // String
+let organization = "XYZCORP";     // String
+let contactNumber = 9876543210;    // Number
+let knowsAutomation = true;        // Boolean
+
+// Declare without assigning 
+let usesPlaywright;                // Undefined
+
+// Verify types using typeof
+console.log("userName:", typeof userName);
+console.log("organization:", typeof organization);
+console.log("contactNumber:", typeof contactNumber);
+console.log("knowsAutomation:", typeof knowsAutomation);
+console.log("usesPlaywright:", typeof usesPlaywright);
